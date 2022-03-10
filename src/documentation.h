ab291e52cb10194390decade7c83f319fa6ad3b4
wail <wail.hassan-babiker@etu.univ-lyon1.fr>
2022-03-10 17:41:39 +0100

    correction pour le script

--- src/documentation.h
+++ src/documentation.h
@@ -9,6 +9,18 @@ Un début de logiciel de gestion d'image pour le cours LIFAP4
 \section Compilation
 Tester sous linux(ubuntu)
 
+\Organisation de l'archive :
+
+/bin : Fichiers executable
+/obj : Fichiers .o
+/src : Fichiers .cpp et .h
+/doc : Documentation
+
+\Compilation :
+Afin de compiler le projet il faudra éxécuter le Makefile.
+En utilisant un terminal dans le répértoire ou se trouve le projet on éxécute la commande make qui va automatiquement compiler le projet.
+
+
 \section Exécution
 
 */
